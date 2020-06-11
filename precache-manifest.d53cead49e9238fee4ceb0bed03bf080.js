2be95e0d687ab01d7bd172478c6e92eb186267d2
pavel <pmykhaleiko@gmail.com>
2020-06-11 23:12:21 +0300

    Updates

---
+++ precache-manifest.d53cead49e9238fee4ceb0bed03bf080.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c1814d173c7ac64f774f5ef62d460616",
+    "url": "/countries-app/index.html"
+  },
+  {
+    "revision": "3c3ede9e0db4e1113e51",
+    "url": "/countries-app/static/css/main.32a7bb72.chunk.css"
+  },
+  {
+    "revision": "a818331db51cc3cd8a02",
+    "url": "/countries-app/static/js/2.8423c81f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/countries-app/static/js/2.8423c81f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3c3ede9e0db4e1113e51",
+    "url": "/countries-app/static/js/main.cb865ce0.chunk.js"
+  },
+  {
+    "revision": "5d2aee138ab7a614c354",
+    "url": "/countries-app/static/js/runtime-main.22ea0ad9.js"
+  },
+  {
+    "revision": "d8b3f08b8a6fafbd91b54fd1515c0afd",
+    "url": "/countries-app/static/media/loader.d8b3f08b.svg"
+  }
+]);
